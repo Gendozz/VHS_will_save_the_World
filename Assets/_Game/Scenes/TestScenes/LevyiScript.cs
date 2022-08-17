a5b62844ec48fc10e3cf6704326cac0cb1cfae16
IAmTau <ilia6478@gmail.com>
2022-08-18 01:42:25 +0500

    testScript

--- Assets/_Game/Scenes/TestScenes/LevyiScript.cs
+++ Assets/_Game/Scenes/TestScenes/LevyiScript.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevyiScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private float _testik;
 }
