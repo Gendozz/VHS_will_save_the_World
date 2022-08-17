03e27bd5ff93b0c0555550f582099bb146e9fc41
Gennadiy_M <gendozz04@gmail.com>
2022-08-17 23:42:08 +0300

    TestScript2

--- Assets/_Game/Scenes/TestScenes/LevyiScript.cs
+++ Assets/_Game/Scenes/TestScenes/LevyiScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class LevyiScript : MonoBehaviour
 {
+    private float _field;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Comment
     }
 
     // Update is called once per frame
