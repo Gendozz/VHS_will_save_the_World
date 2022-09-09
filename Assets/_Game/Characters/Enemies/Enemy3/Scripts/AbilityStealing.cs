1cab0e37654504255e2b156b1e67d4008a7bb7c6
IAmTau <ilia6478@gmail.com>
2022-09-10 00:54:00 +0300

    fixAbilitiStilling + AnimE1andE3

--- Assets/_Game/Characters/Enemies/Enemy3/Scripts/AbilityStealing.cs
+++ Assets/_Game/Characters/Enemies/Enemy3/Scripts/AbilityStealing.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class AbilityStealing : MonoBehaviour
 {
+    [HideInInspector] public bool IsStartTimerBreakDoors = false;
+
     [SerializeField] private float _timeDoubleJump;
     [SerializeField] private float _timeBreakingDoors;
     [SerializeField] private PlayerMovement _playerMovement;
-    [SerializeField] private BreakDoor _breakDoor;
 
     private float _timer = 0;
     private bool _isStartTimerJump = false;
-    private bool _isStartTimerBreakDoors = false;
 
     private void Update()
     {
@@ -23,12 +23,11 @@ public class AbilityStealing : MonoBehaviour
             _timer += Time.deltaTime;
         }
 
-        if (_isStartTimerBreakDoors)
+        if (IsStartTimerBreakDoors)
         {
             if (_timer > _timeBreakingDoors)
             {
-                _isStartTimerBreakDoors = false;
-                _breakDoor.GetAbiluty(false);
+                IsStartTimerBreakDoors = false;
             }
             _timer += Time.deltaTime;
         }
@@ -39,14 +38,13 @@ public class AbilityStealing : MonoBehaviour
         _timer = 0;
         _playerMovement.SetDoubleJumpAbility(true);
         _isStartTimerJump = true;
-        _isStartTimerBreakDoors = false;
+        IsStartTimerBreakDoors = false;
     }
 
     public void StartTimerBreakingDoors()
     {
         _timer = 0;
-        _breakDoor.GetAbiluty(true);
-        _isStartTimerBreakDoors = true;
+        IsStartTimerBreakDoors = true;
         _isStartTimerJump = false;
     }
 }
--- Assets/_Game/Characters/Enemies/Enemy3/Scripts/BreakDoor.cs
+++ Assets/_Game/Characters/Enemies/Enemy3/Scripts/BreakDoor.cs
@@ -2,19 +2,14 @@ using UnityEngine;
 
 public class BreakDoor : MonoBehaviour
 {
+    [SerializeField] private AbilityStealing _abilityStealing;
     [SerializeField] private GameObject _door;
-    private bool _isHaveAbility;
 
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.R) && _isHaveAbility && other.GetComponent<PlayerInput>())
+        if (Input.GetKeyDown(KeyCode.R) && _abilityStealing.IsStartTimerBreakDoors && other.GetComponent<PlayerInput>())
         {
             Destroy(_door);
         }
-    }
-
-    public void GetAbiluty(bool trigger)
-    {
-        _isHaveAbility = trigger;
     }
 }
